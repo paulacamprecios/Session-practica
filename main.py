f10e45c37906528c10c86c10052fda66e94bb0bd
iragonz <92115755+iragonz@users.noreply.github.com>
2025-04-29 19:42:02 +0200

    Actualizar main.py
    Actualizo main.py con mi linea

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 print("Soy Paula Campreciós y durante el apagón tuve que volver a casa ya que estaba en la oficina")
+print("¡Hola, soy Iván, ayer durante el apagón aproveché para echarme una siesta!")
