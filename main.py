748543a22b7cf8ddcf3e9087741f830137c85e80
Ana Belen <anabbre@hotmail.com>
2025-04-29 20:04:28 +0200

    Añadido comentario de nuevo

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-print("Soy Paula Campreciós y durante el apagón me fui a hacer un café")
-print("Soy Paula Campreciós y durante el apagón tuve que volver a casa ya que estaba en la oficina")
-print("¡Hola, soy Iván, ayer durante el apagón aproveché para echarme una siesta!")
-print("Ayer durante el apagón escuché la radio como todo el  mundo") #Comentario de victordani
-print("Ayer durante el apagon estuve leyendo")
-
+print("Soy Ana Belén y ayer estuve leyendo")
